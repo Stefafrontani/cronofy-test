184b2ffb07e7c92fbe73d7b23a6216fda4dc4f37
stefafrontani <stefafrontani@gmail.com>
2021-10-26 15:58:02 -0300

    Actions a basic flow - Cronofy auth, invite atendee and create event

--- client/src/actions/cronofy.js
+++ client/src/actions/cronofy.js
@@ -1,17 +1,67 @@
 const axios = require('axios');
 
-const getElementToken = (cronofyUserSub) => {
-  console.log('getElementToken');
-  const headers = { "Content-Type": "application/json; charset=utf-8" }
+const getUserInfo = (userId) => {
+  console.log('getUserInfo');
+  const headers = {
+    "Content-Type": "application/json; charset=utf-8"
+  }
 
-  axios.post(`${process.env.REACT_APP_COLONY_API_URL}/elementToken`, { cronofyUserSub }, { headers })
-    .then(async (response) => {
-      const responseData = response.data;
-      console.log(responseData);
+  return axios.get(`${process.env.REACT_APP_COLONY_API_URL}/users/${userId}/info`, { headers })
+    .then((response) => {
+      const userInfo = response.data;
+      return userInfo
     })
    .catch(err => console.log(err))
 }
 
+const refreshToken = (userId) => {
+  console.log('refreshToken');
+  const headers = {
+    "Content-Type": "application/json; charset=utf-8"
+  }
+
+  const body = { userId }
+  return axios.post(`${process.env.REACT_APP_COLONY_API_URL}/oauth/token/refresh`, body, { headers })
+    .then((response) => {
+      const userInfo = response.data;
+      localStorage.setItem('cronofyUser', JSON.stringify(userInfo))
+      return userInfo
+    })
+   .catch(err => console.log(err))
+}
+
+const getElementToken = (subs, permissions) => {
+  console.log('getElementToken');
+  const headers = { "Content-Type": "application/json; charset=utf-8" }
+
+  return axios.post(`${process.env.REACT_APP_COLONY_API_URL}/elementToken`, { subs, permissions }, { headers })
+    .then((response) => {
+      const elementToken = response.data;
+      console.log(elementToken)
+      return elementToken
+    })
+   .catch(err => console.log(err))
+}
+
+const createEvent = (slot) => {
+  console.log('createEvent');
+  const { start, end, participants } = slot;
+  const body = { slot }
+  const headers = { "Content-Type": "application/json; charset=utf-8" }
+
+  return axios.post(`${process.env.REACT_APP_COLONY_API_URL}/events`, body, { headers })
+    .then((response) => {
+      const responseData = response.data;
+      console.log(responseData)
+      return responseData
+    })
+   .catch(err => console.log(err))
+}
+
+
 export const cronofyActions = {
-  getElementToken
+  getUserInfo,
+  refreshToken,
+  getElementToken,
+  createEvent
 };
---
+++ client/src/actions/user.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+
+const getUsers = () => {
+  console.log('getUsers');
+  const headers = { "Content-Type": "application/json; charset=utf-8" }
+
+  return axios.get(`${process.env.REACT_APP_COLONY_API_URL}/users`, { headers })
+    .then((response) => {
+      const users = response.data;
+      const usersFormatted = users.map((user) => {
+        const defaultAcc = user.profiles ? `${user.sub} - ` : user.sub;
+        return ({
+          ...user,
+          description: user.profiles && user.profiles.length > 0 ?
+            user.profiles.reduce((acc, curr) => {
+
+              const { profileName } = curr || '';
+              return `${acc} ${profileName} `
+            }, defaultAcc)
+            : 'No profiles associated'
+        })
+      })
+      return usersFormatted;
+    })
+   .catch(err => console.log(err))
+}
+
+const getUserById = (userId) => {
+  console.log('getUserById');
+  const headers = { "Content-Type": "application/json; charset=utf-8" }
+
+  return axios.get(`${process.env.REACT_APP_COLONY_API_URL}/users/${userId}`, { headers })
+    .then(async (response) => {
+      const userFound = response.data;
+      return userFound;
+    })
+   .catch(err => console.log(err))
+}
+
+export const userActions = {
+  getUsers,
+  getUserById
+};
