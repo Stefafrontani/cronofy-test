4c0f02a5cbd60ab018e6e6d1b3a70d58c78f4ba7
stefafrontani <stefafrontani@gmail.com>
2021-10-26 15:59:27 -0300

    Add variable to avoid linking accounts

--- client/src/pages/SignInPage/SignInPage.jsx
+++ client/src/pages/SignInPage/SignInPage.jsx
@@ -8,7 +8,7 @@ const SignInPage = () => {
   return (
     <a
       className="App-link"
-      href={`${REACT_APP_CRONOFY_AUTH_BASE_URL}?response_type=code&client_id=${REACT_APP_CRONOFY_CLIENT_ID}&redirect_uri=${REACT_APP_CRONOFY_REDIRECT_URI}&scope=${'create_calendar read_events create_event  delete_event  read_free_busy change_participation_status'}`}
+      href={`${REACT_APP_CRONOFY_AUTH_BASE_URL}?avoid_linking=true&response_type=code&client_id=${REACT_APP_CRONOFY_CLIENT_ID}&redirect_uri=${REACT_APP_CRONOFY_REDIRECT_URI}&scope=${'create_calendar read_events create_event  delete_event  read_free_busy change_participation_status'}`}
       /* target="_blank" */
       rel="noopener noreferrer"
     >
