1ce917a3b61aa0f367e67d4df65508444411373c
stefafrontani <stefafrontani@gmail.com>
2021-10-26 15:56:21 -0300

    utility to create table withinforation

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,52 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
+window.createTable = (config) => {
+  const headers = config.headers;
+  const rows = config.rows;
+  return (
+    <table>A
+      <thead>
+        <tr>
+          {headers.map((header, index)=> {
+            return (
+              <td key={`header--${index}`} data-tablecolumn={header}>{header.toUpperCase()}</td>
+            )
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {
+          rows.map((row, index) => {
+            return (
+              <tr key={`row--${index}`}>
+                {
+                  row.map((cellValue, index) => {
+                    return (
+                      <td key={`cellValue--${index}`} data-tablecolumn={headers[index]}>
+                        {cellValue instanceof Array
+                          ? cellValue.map((p, index) => {
+                            return <p key={`p--${index}`}>{p}</p>
+                          })
+                          : cellValue instanceof Object
+                            ? React.createElement(cellValue.component, { ...cellValue.props }, cellValue.html)
+                            : cellValue
+                        }
+                      </td>
+                    )
+                  })
+                }
+              </tr>
+            ) 
+          })
+        }
+        <tr>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
