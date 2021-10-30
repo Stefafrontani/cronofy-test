e755d61c4e2404908a151cb3183fb27c81493c18
stefafrontani <stefafrontani@gmail.com>
2021-10-30 11:37:39 -0300

    Add helpers for table configuration

---
+++ client/src/helpers/tablesConfig/index.js
@@ -0,0 +1,7 @@
+import { dateTimePickerPageTablesConfig } from './dateTimePickerPage';
+import { endpointsPageTablesConfig } from './endpointsPage';
+
+export const tablesConfig = {
+  dateTimePickerPageTablesConfig: dateTimePickerPageTablesConfig,
+  endpointsPageTablesConfig: endpointsPageTablesConfig,
+};
