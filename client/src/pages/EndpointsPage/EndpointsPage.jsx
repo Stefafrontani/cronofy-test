7768334480e9c7c547648d8168451974aaf9d8a8
stefafrontani <stefafrontani@gmail.com>
2021-10-30 11:40:56 -0300

    Adapt EndpointsPage to new table config helper

--- client/src/pages/EndpointsPage/EndpointsPage.jsx
+++ client/src/pages/EndpointsPage/EndpointsPage.jsx
@@ -1,66 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { cronofyActions, userActions } from '../../actions';
-import './EndpointsPage.css'
+import { tablesConfig } from '../../helpers';
+import './EndpointsPage.css';
+
+const { endpointsPageTablesConfig } = tablesConfig;
+
 
 const { getUsers, getUserById } = userActions;
 const { getUserInfo, refreshToken } = cronofyActions;
-
-const headers = [ "url", "description", "cronofy-client-options", "cronofy-client-method", "cronofy-client-method-options", "query", "body", "call"];
-const cronofyEndpointsRows = [
-  [
-    "GET: /users/3/info",
-    ["Cronofy endpoint:", "Request to return user info"],
-    "----",
-    "----",
-    "----",
-    "userId",
-    "----",
-    {
-      html: "GET USER INFO",
-      component: 'button',
-      props: {}
-    }
-  ],
-  [
-    "GET: /oauth/token/refresh",
-    ["Cronofy endpoint", "request to refresh access token"],
-    "----",
-    "----",
-    "----",
-    "----",
-    "userId",
-    {
-      html: "REFRESH TOKEN",
-      component: 'button',
-      props: {}
-    }
-  ]
-]
-const appEndpointsRows = [
-  [
-    "GET: /users",
-    ["App endpoint", "Request to get users", "Done on mount"],
-    "----",
-    "----",
-    "----",
-    "userId",
-    "----",
-    {
-      html: "GET USERS",
-      component: 'button',
-      props: {}
-    }
-  ]
-]
-
-const cronofyEndpointsTableConfig = {
-  "headers": headers,
-  rows: cronofyEndpointsRows
-}
-const appEndpointsTableConfig = {
-  "headers": headers,
-  rows: appEndpointsRows
-}
 
 const EndpointsPage = () => {
   const [users, setUsers] = useState([]);
@@ -107,10 +54,10 @@ const EndpointsPage = () => {
     handleGetUsers();
   }, []);
 
-  cronofyEndpointsTableConfig.rows[0][cronofyEndpointsTableConfig.rows[0].length - 1].props = { onClick: handleGetUserInfo }
-  cronofyEndpointsTableConfig.rows[1][cronofyEndpointsTableConfig.rows[1].length - 1].props = { onClick: handleRefreshToken }
+  endpointsPageTablesConfig.cronofy.rows[0][endpointsPageTablesConfig.cronofy.rows[0].length - 1].props = { onClick: handleGetUserInfo }
+  endpointsPageTablesConfig.cronofy.rows[1][endpointsPageTablesConfig.cronofy.rows[1].length - 1].props = { onClick: handleRefreshToken }
   
-  appEndpointsTableConfig.rows[0][appEndpointsTableConfig.rows[0].length - 1].props = { onClick: handleGetUsers }
+  endpointsPageTablesConfig.app.rows[0][endpointsPageTablesConfig.app.rows[0].length - 1].props = { onClick: handleGetUsers }
 
   return (
     <div className="endpointsPage">
@@ -129,10 +76,10 @@ const EndpointsPage = () => {
         }
       </div>
       <h2>Cronofy ENDPOINTS</h2>
-      {window.createTable(cronofyEndpointsTableConfig)};
+      {window.createTable(endpointsPageTablesConfig.cronofy)};
       <h2>APP ENDPOINTS</h2>
       <h3>USERS</h3>
-      {window.createTable(appEndpointsTableConfig)};
+      {window.createTable(endpointsPageTablesConfig.app)};
     </div>
   );
 }
