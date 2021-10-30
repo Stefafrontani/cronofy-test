1a130cea4ef7f38500eb728de8d417a503f864dd
Giacomo Frontani <gfrontani@NTBK-TECH880.fibertel.com.ar>
2021-10-30 20:08:56 -0300

    Create events table migration file

--- server/migrations/1635606989170_add-event-model.js
+++ server/migrations/1635606989170_add-event-model.js
@@ -5,5 +5,30 @@ exports.shorthands = undefined;
 exports.up = pgm => {
   pgm.createTable('events', {
     id: 'id',
+    subscriptionCallbackUrl: {
+      type: 'varchar(1000)',
+      notNull: false
+    },
+    summary: {
+      type: 'varchar(1000)',
+      notNull: false
+    },
+    description: {
+      type: 'varchar(1000)',
+      notNull: false
+    },
+    start: {
+      type: 'TIMESTAMP with time zone',
+      notNull: false
+    },
+    end: {
+      type: 'TIMESTAMP with time zone',
+      notNull: false
+    },
+    status: {
+      type: 'varchar(1000)',
+      notNull: true,
+      default: 'tentative'
+    }
   });
 };
