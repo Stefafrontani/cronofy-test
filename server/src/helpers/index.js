6e1786f40137b9fe2b9399ebd693e98a589c66d7
stefafrontani <stefafrontani@gmail.com>
2021-11-03 14:40:20 -0300

    Create candidates helpers functions

--- server/src/helpers/index.js
+++ server/src/helpers/index.js
@@ -1,11 +1,13 @@
 const auth = require('./auth');
+const candidates = require('./candidates');
 const events = require('./events');
 const querys = require('./querys');
 const users = require('./users');
 
 module.exports = {
   auth,
+  candidates,
   events,
   querys,
-  users
+  users,
 }
