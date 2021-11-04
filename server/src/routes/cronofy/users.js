bd895d9e8b6f6a381557d71f7c2e2ace5de0daf0
stefafrontani <stefafrontani@gmail.com>
2021-11-03 22:32:42 -0300

    Add routes to get all not channels and delete

--- server/src/routes/cronofy/users.js
+++ server/src/routes/cronofy/users.js
@@ -1,8 +1,8 @@
 const { Router } =  require('express');
 const router = Router();
 
-const { users } = require('../../controllers/cronofy')
-const { getUserInfoRoute } = users;
+const { users } = require('../../controllers/cronofy');
+const { getUserInfoRoute, getUserNotificationsChannelsRoute, deleteNotificationsChannelRoute } = users;
 
 router.use('*', (req, res, next) => {
   console.log('/users');
@@ -10,5 +10,7 @@ router.use('*', (req, res, next) => {
 });
 
 router.get('/:userId/info', getUserInfoRoute);
+router.get('/:userId/notifications', getUserNotificationsChannelsRoute);
+router.delete('/:userId/notifications/:channelId', deleteNotificationsChannelRoute);
 
 module.exports = router;
