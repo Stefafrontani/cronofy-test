e873608c7d530197252906beb98d6343629b6cc2
stefafrontani <stefafrontani@gmail.com>
2021-11-05 09:53:45 -0300

    Add events page navigation

--- client/src/Navigation.jsx
+++ client/src/Navigation.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
-import SignInPage from './pages/SignInPage/SignInPage';
-import CronofyAuthorizationPage from './pages/CronofyAuthorizationPage/CronofyAuthorizationPage';
 import CalendarSyncPage from './pages/CalendarSyncPage/CalendarSyncPage';
-import DateTimePickerPage from './pages/DateTimePickerPage/DateTimePickerPage';
 import CreateEventPage from './pages/CreateEventPage/CreateEventPage';
+import CronofyAuthorizationPage from './pages/CronofyAuthorizationPage/CronofyAuthorizationPage';
+import DateTimePickerPage from './pages/DateTimePickerPage/DateTimePickerPage';
 import EndpointsPage from './pages/EndpointsPage/EndpointsPage';
+import EventsPage from './pages/EventsPage/EventsPage';
+import SignInPage from './pages/SignInPage/SignInPage';
 
 const Navigation = () => {
   return (
     <Switch>
-      <Route component={SignInPage} path="/login" />
       <Route component={CalendarSyncPage} path="/calendarSync" />
+      <Route component={CreateEventPage} path="/createEvent" />
+      <Route component={CronofyAuthorizationPage} path="/oauth/callback" />
       <Route component={DateTimePickerPage} path="/dateTimePicker" />
       <Route component={EndpointsPage} path="/endpoints" />
-      <Route component={CronofyAuthorizationPage} path="/oauth/callback" />
-      <Route component={CreateEventPage} path="/createEvent" />
+      <Route component={EventsPage} path="/events" />
+      <Route component={SignInPage} path="/login" />
     </Switch>
   );
 }
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,10 +6,11 @@ const Header = () => {
     <header style={{ padding: '20px' }}>
       <nav>
         <ul style={{ display: 'flex' }}>
-          <li><Link to="/login">LOGIN</Link></li>
           <li><Link to="/calendarSync">CALENDAR SYNC</Link></li>
           <li><Link to="/dateTimePicker">DATE TIME PICKER</Link></li>
           <li><Link to="/endpoints">ENDPOINTS</Link></li>
+          <li><Link to="/events">EVENTS</Link></li>
+          <li><Link to="/login">LOGIN</Link></li>
         </ul>
       </nav>
     </header>
